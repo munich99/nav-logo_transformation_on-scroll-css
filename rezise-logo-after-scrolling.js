8fc1b714633ff82f5ea7d257a2f171e9dd290099
munich99 <regerstrasse@icloud.com>
2019-10-04 13:45:58 +0200

    resize effect on scroll

--- rezise-logo-after-scrolling.js
+++ rezise-logo-after-scrolling.js
@@ -5,12 +5,12 @@ document.getElementById("myDIV").onscroll = function() {myFunction()};
 
 function myFunction() {
     y = window.scrollY;
-    if (y>250 && scroll==true) {        
-        console.log(y); 
+    if (y>250 && scroll==true) {
         scroll = false;
-    }
+        var element = document.getElementById("Scroll");
+        element.classList.add("myscroll");        
+    }    
     if (y<250  && scroll==false) { 
-        scroll = true; 
-        console.log("small");
+        scroll = true;        
     }
 }
