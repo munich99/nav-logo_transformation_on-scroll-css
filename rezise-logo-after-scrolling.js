346a00bbb41f229132973e17e92b8c566d4102a1
munich99 <regerstrasse@icloud.com>
2019-10-04 20:40:47 +0200

    animamate color effect

--- rezise-logo-after-scrolling.js
+++ rezise-logo-after-scrolling.js
@@ -1,14 +1,17 @@
 var y;
 var scroll = true;
 
-document.getElementById("myDIV").onscroll = function() {myFunction()};
+document.getElementById("myDIV").onscroll = function() { myFunction() };
 
 function myFunction() {
     y = window.scrollY;
     if (y>250 && scroll==true) {
         scroll = false;
         var element = document.getElementById("Scroll");
-        element.classList.add("myscroll");        
+        element.classList.add("myscroll");    
+        
+        var element = document.getElementById("mynav");
+        element.classList.add("myscrollNav"); 
     }    
     if (y<250  && scroll==false) { 
         scroll = true;        
