2a1942e130bd0f4cf261374d86e443ed538fd316
munich99 <regerstrasse@icloud.com>
2019-10-05 18:29:12 +0200

    smarter with querySelector

--- rezise-logo-after-scrolling.js
+++ rezise-logo-after-scrolling.js
@@ -6,21 +6,29 @@ document.getElementById("myDIV").onscroll = function() { myFunction() };
 function myFunction() {
     y = window.scrollY;
     if (y>250 && scroll==true) {
-        scroll = false;
-        var element = document.getElementById("Scroll");
-        element.classList.add("myscroll");    
         
-        var element = document.getElementById("mynav");
-        element.classList.add("myscrollNav"); 
+        document.querySelector("#Scroll").
+        style ='transform: scale(1) translateY(6px); fill: black;';  
+
+        document.querySelector("#Stroke").
+        style='stroke:black';       
 
         for (var i = 0; i < document.getElementsByClassName("nav-link").length; i++) {
 
             var element = document.getElementsByClassName("nav-link")[i];
             element.classList.add("myscrollNavcolor"); 
         }
+
+        scroll = false;
+        console.log(scroll + "sollte wieder");
+        
     }    
 
-    if (y<250  && scroll==false) { 
-        scroll = true;        
+    if (y<250  && scroll==false) {            
+        document.querySelector("#Scroll").
+        style ='transform: scale(5) translateY(10vh); fill: white;';
+        document.querySelector("#Stroke").
+        style='stroke:white';
+        scroll = true;   
     }
 }
