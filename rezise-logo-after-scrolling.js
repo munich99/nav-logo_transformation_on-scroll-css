d10bdf924fe5a3caca1b6a7008e74c70c9114ab6
munich99 <regerstrasse@icloud.com>
2019-10-05 19:02:51 +0200

    smart solution finish

--- rezise-logo-after-scrolling.js
+++ rezise-logo-after-scrolling.js
@@ -8,13 +8,15 @@ function myFunction() {
     if (y>250 && scroll==true) {
         
         document.querySelector("#Scroll").
-        style ='transform: scale(1) translateY(6px); fill: black;';  
+        style ='transform: scale(1) translateY(6px); fill: #354731;';  
 
         document.querySelector("#Stroke").
-        style='stroke:black';       
+        style='stroke:#354731';
+
+        document.querySelector("#mynav").
+        style='background-color:white!important;';
 
         for (var i = 0; i < document.getElementsByClassName("nav-link").length; i++) {
-
             var element = document.getElementsByClassName("nav-link")[i];
             element.classList.add("myscrollNavcolor"); 
         }
@@ -27,8 +29,18 @@ function myFunction() {
     if (y<250  && scroll==false) {            
         document.querySelector("#Scroll").
         style ='transform: scale(5) translateY(10vh); fill: white;';
+
         document.querySelector("#Stroke").
         style='stroke:white';
+
+        document.querySelector("#mynav").
+        style='background-color:#354731!important;';
+
+        for (var i = 0; i < document.getElementsByClassName("nav-link").length; i++) {
+            var element = document.getElementsByClassName("nav-link")[i];
+            element.classList.remove("myscrollNavcolor"); 
+        }
+
         scroll = true;   
     }
 }
